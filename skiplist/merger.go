4ff285f6852aff27c069ceb168424185f1bc13cf
Sarath Lakshman <sarathlakshman@slynux.com>
2015-11-05 06:45:04 +0530

    skiplist: Add k-way iterator merger

---
+++ skiplist/merger.go
@@ -0,0 +1,97 @@
+package skiplist
+
+import "container/heap"
+
+type mIterator struct {
+	iters []*Iterator
+	h     nodeHeap
+	curr  *Node
+}
+
+type heapItem struct {
+	iter *Iterator
+	n    *Node
+}
+
+type nodeHeap []heapItem
+
+func (h nodeHeap) Len() int           { return len(h) }
+func (h nodeHeap) Less(i, j int) bool { return h[i].iter.cmp(h[i].n.Item(), h[j].n.Item()) < 0 }
+func (h nodeHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *nodeHeap) Push(x interface{}) {
+	*h = append(*h, x.(heapItem))
+}
+
+func (h *nodeHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func NewMergeIterator(iters []*Iterator) *mIterator {
+	return &mIterator{
+		iters: iters,
+	}
+}
+
+func (mit *mIterator) SeekFirst() {
+	for _, it := range mit.iters {
+		it.SeekFirst()
+		if it.Valid() {
+			n := it.GetNode()
+			mit.h = append(mit.h, heapItem{iter: it, n: n})
+		}
+	}
+
+	heap.Init(&mit.h)
+	mit.Next()
+}
+
+func (mit *mIterator) Valid() bool {
+	return mit.curr != nil
+}
+
+func (mit *mIterator) Next() {
+	mit.curr = nil
+	if mit.h.Len() == 0 {
+		return
+	}
+
+	o := heap.Pop(&mit.h)
+	hi := o.(heapItem)
+	mit.curr = hi.n
+	hi.iter.Next()
+	if hi.iter.Valid() {
+		hi.n = hi.iter.GetNode()
+		heap.Push(&mit.h, hi)
+	}
+}
+
+func (mit *mIterator) Seek(itm Item) bool {
+	var found bool
+	for _, it := range mit.iters {
+		if it.Seek(itm) {
+			found = true
+		}
+		if it.Valid() {
+			n := it.GetNode()
+			mit.h = append(mit.h, heapItem{iter: it, n: n})
+		}
+	}
+
+	heap.Init(&mit.h)
+	mit.Next()
+
+	return found
+}
+
+func (mit *mIterator) Get() Item {
+	return mit.curr.Item()
+}
+
+func (mit *mIterator) GetNode() *Node {
+	return mit.curr
+}
---
+++ skiplist/merger_test.go
@@ -0,0 +1,48 @@
+package skiplist
+
+import "testing"
+import "fmt"
+
+func TestMerger(t *testing.T) {
+	var lists []*Skiplist
+	var iters []*Iterator
+
+	s := New()
+	cmp := CompareBytes
+	buf := s.MakeBuf()
+	defer s.FreeBuf(buf)
+
+	n := 5
+
+	for i := 0; i < n; i++ {
+		lists = append(lists, New())
+	}
+
+	for i := 0; i < 10000; i++ {
+		if i >= 1000 && i <= 8000 && i%n == 0 {
+			continue
+		}
+		s := lists[i%n]
+		s.Insert(NewByteKeyItem([]byte(fmt.Sprintf("%010d", i))), cmp, buf)
+	}
+
+	for i := 0; i < n; i++ {
+		buf := s.MakeBuf()
+		iters = append(iters, lists[i].NewIterator(cmp, buf))
+	}
+
+	mit := NewMergeIterator(iters)
+
+	i := 0
+	for mit.SeekFirst(); mit.Valid(); i++ {
+		if i >= 1000 && i <= 8000 && i%n == 0 {
+			continue
+		}
+		expected := fmt.Sprintf("%010d", i)
+		got := string(*(mit.Get().(*byteKeyItem)))
+		if got != expected {
+			t.Errorf("Expected %s, got %v", expected, got)
+		}
+		mit.Next()
+	}
+}
