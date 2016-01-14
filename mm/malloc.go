b73dffdefe64dc15bc9bdc86826fd9f96bafbb2a
Sarath Lakshman <sarathlakshman@slynux.com>
2016-01-14 15:41:34 +0530

    mm: Integrate jemalloc
    Jemalloc needs to be build with "je_" prefix and following env needs to be set.
    export CGO_CFLAGS="-I/Users/sarath/repos/jemalloc/install_path/include"
    export CGO_LDFLAGS="-L/Users/sarath/repos/jemalloc/install_path/lib -ljemalloc"

--- mm/malloc.go
+++ mm/malloc.go
@@ -1,6 +1,38 @@
 package mm
 
-// #include <stdlib.h>
+/*
+#include <stdlib.h>
+#include <jemalloc/jemalloc.h>
+
+typedef struct {
+	char *buf;
+	int offset;
+	int size;
+} stats_buf;
+
+void writecb(void *ref, const char *s) {
+	stats_buf *buf = (stats_buf *)(ref);
+	int len;
+	len = strlen(s);
+	if (buf->offset + len >= buf->size) {
+		buf->size *=2;
+		buf->buf = realloc(buf->buf, buf->size);
+	}
+	strncpy(buf->buf + buf->offset, s, len);
+	buf->offset += len;
+}
+
+char * doStats()  {
+	stats_buf buf;
+	buf.size = 1024;
+	buf.buf = malloc(buf.size);
+	buf.offset = 0;
+	je_malloc_stats_print(writecb, &buf, NULL);
+	buf.buf[buf.offset] = 0;
+	return buf.buf;
+}
+
+*/
 import "C"
 
 import (
@@ -8,9 +40,16 @@ import (
 )
 
 func Malloc(l int) unsafe.Pointer {
-	return C.malloc(C.size_t(l))
+	return C.je_malloc(C.size_t(l))
 }
 
 func Free(p unsafe.Pointer) {
-	C.free(p)
+	C.je_free(p)
+}
+
+func Stats() string {
+	buf := C.doStats()
+	s := C.GoString(buf)
+	C.free(unsafe.Pointer(buf))
+	return s
 }
