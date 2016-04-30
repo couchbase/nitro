914174f58d115fb2d4929d1d4f0b97e7117c132f
Sarath Lakshman <sarathlakshman@slynux.com>
2016-04-30 13:02:49 +0530

    skiplist/smr: Fix a bug in SMR algorithm
    The SMR algorithm is based on the assumption that the items from a freelist
    can be reclaimed once all the accessors in its session terminates. But, that
    condition is not sufficient for reclaiming items. There could be multiple
    closed session at any point of time. Let us consider two closed sessions,
    session-1 and session-2. When the last accessor of session-2 goes away,
    it will try to reclaim items from its freelist. But the accessors from
    session-1 are still active and may be able reach items in session-2
    freelist. Hence, a session can only be terminated after termination of
    previous sessions. ie, session n can only be terminated after terminating
    session (n-1).

--- skiplist/access_barrier.go
+++ skiplist/access_barrier.go
@@ -2,6 +2,7 @@ package skiplist
 
 import (
 	"math"
+	"sync"
 	"sync/atomic"
 	"unsafe"
 )
@@ -21,26 +22,33 @@ import (
 * In this algorithm, the unit of safety period is called barrier session. All the
 * live accessor of the skiplist are tracked in a barrier session. Whenever a
 * skiplist delete or group of deletes is performed, current barrier session is
-* terminator or flushed and new barrier session is started. The previous barrier
-* session tracks all the live accessors until the session of terminated. The right
+* closed and new barrier session is started. The previous barrier
+* session tracks all the live accessors until the session of closed. The right
 * time to safely reclaim the node is when all the accessor becomes dead. This makes
 * sure that unlinked node will be invisible to everyone. The accessors in the
 * barrier session can cooperatively detect and mark when each of them terminates.
-* When the last accessor terminates, it can take the action to call the destructor
-* for the node.
+* When the last accessor leaves, it can take the action to call the destructor
+* for the node and barrier session terminates.
 *
-* Terminating and installing a new barrier session:
+* Closing and installing a new barrier session:
 * A session liveCount is incremented everytime an accessor is entering the skiplist
-* and decremented when the leave the skiplist. When a session is terminated and new
+* and decremented when the leave the skiplist. When a session is closed and new
 * one needs to be installed, we just swap the global barrier session reference.
-* There could be race conditions while a session is being marked as terminated. Still
+* There could be race conditions while a session is being marked as closed. Still
 * an ongoing skiplist accessor can increment the counter of a session which was marked
-* as terminated. To detect those accessors and make them retry, we add a large number
-* to the liveCount as part of the session termination phase. When the accessor finds
+* as closed. To detect those accessors and make them retry, we add a large number
+* to the liveCount as part of the session close phase. When the accessor finds
 * that the incremented result is greater than that large offset, it needs to backoff
 * from the current session and acquire new session to increment the count. In this
 * scheme, whoever decrements the count and gets the count equal to the large offset
 * is responsible for deallocation of the object.
+*
+* The algorithm has to consider one more condition before it can call destructor for
+* the session. Multiple closed sessions can be active at a time. We cannot call the
+* destructor for a closed session while a previous closed session is still not terminated.
+* Because, even through accessors from a closed session has become zero, accessors from previous
+* closed session would be able to access items in the later closed session. Hence, a closed session
+* can be terminated only after termination of all previous closed sessions.
 * */
 
 type BarrierSessionDestructor func(objectRef unsafe.Pointer)
@@ -50,6 +58,15 @@ const barrierFlushOffset = math.MaxInt32 / 2
 type BarrierSession struct {
 	liveCount *int32
 	objectRef unsafe.Pointer
+	seqno     uint64
+	closed    int32
+}
+
+func CompareBS(this, that unsafe.Pointer) int {
+	thisItm := (*BarrierSession)(this)
+	thatItm := (*BarrierSession)(that)
+
+	return int(thisItm.seqno) - int(thatItm.seqno)
 }
 
 func newBarrierSession() *BarrierSession {
@@ -61,17 +78,49 @@ func newBarrierSession() *BarrierSession {
 }
 
 type AccessBarrier struct {
-	session unsafe.Pointer
-	callb   BarrierSessionDestructor
+	activeSeqno uint64
+	session     unsafe.Pointer
+	callb       BarrierSessionDestructor
+
+	freeq               *Skiplist
+	freeSeqno           uint64
+	isDestructorRunning int32
 
 	active bool
+	sync.Mutex
 }
 
 func newAccessBarrier(active bool, callb BarrierSessionDestructor) *AccessBarrier {
-	return &AccessBarrier{
+	ab := &AccessBarrier{
 		active:  active,
 		session: unsafe.Pointer(newBarrierSession()),
 		callb:   callb,
+	}
+	if active {
+		ab.freeq = New()
+	}
+	return ab
+}
+
+func (ab *AccessBarrier) doCleanup() {
+	buf1 := ab.freeq.MakeBuf()
+	buf2 := ab.freeq.MakeBuf()
+	defer ab.freeq.FreeBuf(buf1)
+	defer ab.freeq.FreeBuf(buf2)
+
+	iter := ab.freeq.NewIterator(CompareBS, buf1)
+	defer iter.Close()
+
+	for iter.SeekFirst(); iter.Valid(); iter.Next() {
+		node := iter.GetNode()
+		bs := (*BarrierSession)(node.Item())
+		if bs.seqno != ab.freeSeqno+1 {
+			return
+		}
+
+		ab.freeSeqno++
+		ab.callb(bs.objectRef)
+		ab.freeq.DeleteNode(node, CompareBS, buf2, &ab.freeq.Stats)
 	}
 }
 
@@ -95,9 +144,17 @@ func (ab *AccessBarrier) Release(bs *BarrierSession) {
 	if ab.active {
 		liveCount := atomic.AddInt32(bs.liveCount, -1)
 		if liveCount == barrierFlushOffset {
-			objectRef := bs.objectRef
-			if objectRef != nil && atomic.CompareAndSwapPointer(&bs.objectRef, objectRef, nil) {
-				ab.callb(objectRef)
+			buf := ab.freeq.MakeBuf()
+			defer ab.freeq.FreeBuf(buf)
+
+			// Accessors which entered a closed barrier session steps down automatically
+			// But, they may try to close an already closed session.
+			if atomic.AddInt32(&bs.closed, 1) == 1 {
+				ab.freeq.Insert(unsafe.Pointer(bs), CompareBS, buf, &ab.freeq.Stats)
+				if atomic.CompareAndSwapInt32(&ab.isDestructorRunning, 0, 1) {
+					ab.doCleanup()
+					atomic.CompareAndSwapInt32(&ab.isDestructorRunning, 1, 0)
+				}
 			}
 		}
 	}
@@ -105,17 +162,18 @@ func (ab *AccessBarrier) Release(bs *BarrierSession) {
 
 func (ab *AccessBarrier) FlushSession(ref unsafe.Pointer) {
 	if ab.active {
-	retry:
+		ab.Lock()
+		defer ab.Unlock()
+
 		bsPtr := atomic.LoadPointer(&ab.session)
 		newBsPtr := unsafe.Pointer(newBarrierSession())
-		if atomic.CompareAndSwapPointer(&ab.session, bsPtr, newBsPtr) {
-			bs := (*BarrierSession)(bsPtr)
-			bs.objectRef = ref
+		atomic.CompareAndSwapPointer(&ab.session, bsPtr, newBsPtr)
+		bs := (*BarrierSession)(bsPtr)
+		bs.objectRef = ref
+		ab.activeSeqno++
+		bs.seqno = ab.activeSeqno
 
-			atomic.AddInt32(bs.liveCount, barrierFlushOffset+1)
-			ab.Release(bs)
-		} else {
-			goto retry
-		}
+		atomic.AddInt32(bs.liveCount, barrierFlushOffset+1)
+		ab.Release(bs)
 	}
 }
