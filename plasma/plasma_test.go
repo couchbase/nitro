d2e6e819d02ac6e369cfd7dc64b020f3b04c3cbd
Sarath Lakshman <sarathlakshman@slynux.com>
2016-08-17 14:51:29 +0530

    plasma: Add test for simple plasma operations
    Change-Id: Ic23a523f4f3efd1efb7fd8904f348d6f04f95a2a

---
+++ plasma/plasma_test.go
@@ -0,0 +1,54 @@
+package plasma
+
+import (
+	"github.com/t3rm1n4l/nitro/skiplist"
+	"testing"
+	"unsafe"
+)
+
+func newTestIntPlasmaStore() *Plasma {
+	cfg := Config{
+		MaxDeltaChainLen: 200,
+		MaxPageItems:     400,
+		MinPageItems:     25,
+		Compare:          skiplist.CompareInt,
+		ItemSize: func(unsafe.Pointer) uintptr {
+			return unsafe.Sizeof(new(skiplist.IntKeyItem))
+		},
+	}
+	return New(cfg)
+}
+
+func TestPlasmaSimple(t *testing.T) {
+	s := newTestIntPlasmaStore()
+	w := s.NewWriter()
+	for i := 0; i < 1000000; i++ {
+		w.Insert(skiplist.NewIntKeyItem(i))
+	}
+
+	for i := 0; i < 1000000; i++ {
+		itm := skiplist.NewIntKeyItem(i)
+		got := w.Lookup(itm)
+		if skiplist.CompareInt(itm, got) != 0 {
+			t.Errorf("mismatch %d != %d", i, skiplist.IntFromItem(got))
+		}
+	}
+
+	for i := 0; i < 800000; i++ {
+		w.Delete(skiplist.NewIntKeyItem(i))
+	}
+
+	for i := 0; i < 800000; i++ {
+		itm := skiplist.NewIntKeyItem(i)
+		got := w.Lookup(itm)
+		if i < 800000 {
+			if got != nil {
+				t.Errorf("Expected missing %d", i)
+			}
+		} else {
+			if skiplist.CompareInt(itm, got) != 0 {
+				t.Errorf("Expected %d, got %d", i, skiplist.IntFromItem(got))
+			}
+		}
+	}
+}
