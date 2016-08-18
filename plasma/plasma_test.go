7bb8e3089cc5f9ed4634a6d14c939d29af8cb83d
Sarath Lakshman <sarathlakshman@slynux.com>
2016-08-18 12:30:10 +0530

    plasma: Add test for plasma iterator
    Change-Id: I9436e55a1d707d666bc9baa665f7e180091d2f09

--- plasma/plasma_test.go
+++ plasma/plasma_test.go
@@ -172,3 +172,80 @@ func TestPlasmaLookupPerf(t *testing.T) {
 	fmt.Println(s.GetStats())
 	fmt.Printf("%d items lookup took %v -> %v items/s\n", total, dur, float64(total)/float64(dur.Seconds()))
 }
+
+func TestIteratorSimple(t *testing.T) {
+	s := newTestIntPlasmaStore()
+	w := s.NewWriter()
+	for i := 0; i < 1000000; i++ {
+		w.Insert(skiplist.NewIntKeyItem(i))
+	}
+
+	i := 0
+	itr := s.NewIterator()
+	for itr.SeekFirst(); itr.Valid(); itr.Next() {
+		if v := skiplist.IntFromItem(itr.Get()); v != i {
+			t.Errorf("expected %d, got %d", i, v)
+		}
+
+		i++
+	}
+
+	if i != 1000000 {
+		t.Errorf("expected %d, got %d", i)
+	}
+
+}
+
+func TestIteratorSeek(t *testing.T) {
+	s := newTestIntPlasmaStore()
+	w := s.NewWriter()
+	for i := 0; i < 1000000; i++ {
+		w.Insert(skiplist.NewIntKeyItem(i))
+	}
+
+	itr := s.NewIterator().(*Iterator)
+	for i := 0; i < 1000000; i++ {
+		itr.Seek(skiplist.NewIntKeyItem(i))
+		if v := skiplist.IntFromItem(itr.Get()); v != i {
+			t.Errorf("expected %d, got %d", i, v)
+		}
+	}
+}
+
+func TestPlasmaIteratorLookupPerf(t *testing.T) {
+	var wg sync.WaitGroup
+
+	numThreads := 8
+	n := 10000000
+	nPerThr := n / numThreads
+	s := newTestIntPlasmaStore()
+	total := numThreads * nPerThr
+
+	for i := 0; i < numThreads; i++ {
+		wg.Add(1)
+		w := s.NewWriter()
+		go doInsert(w, &wg, i, nPerThr)
+	}
+	wg.Wait()
+
+	t0 := time.Now()
+	for i := 0; i < numThreads; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, id, n int) {
+			defer wg.Done()
+			itr := s.NewIterator()
+
+			for i := 0; i < n; i++ {
+				val := i + id*n
+				itm := skiplist.NewIntKeyItem(val)
+				itr.Seek(itm)
+			}
+		}(&wg, i, nPerThr)
+	}
+	wg.Wait()
+
+	dur := time.Since(t0)
+
+	fmt.Println(s.GetStats())
+	fmt.Printf("%d items iterator seek took %v -> %v items/s\n", total, dur, float64(total)/float64(dur.Seconds()))
+}
