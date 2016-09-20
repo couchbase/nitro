562eb22ed74ac5eb420aee90de020f5a5a7c274c
Sarath Lakshman <sarathlakshman@slynux.com>
2016-09-20 17:24:14 +0530

    plasma: Add blocktype in lss blocks
    Change-Id: I0a7afa147c67101d258d1ceb5ac2d8673ac5dc96

--- plasma/persistor.go
+++ plasma/persistor.go
@@ -1,6 +1,22 @@
 package plasma
 
+import (
+	"encoding/binary"
+	"unsafe"
+)
+
 var maxPageEncodedSize = 1024 * 1024 * 10
+
+type lssBlockType uint16
+
+var lssBlockTypeSize = int(unsafe.Sizeof(*(new(lssBlockType))))
+
+const (
+	lssPageData lssBlockType = iota
+	lssPageAlloc
+	lssPageDelete
+	lssDiscard
+)
 
 func (s *Plasma) PersistAll() {
 	buf := make([]byte, maxPageEncodedSize)
@@ -10,15 +26,16 @@ func (s *Plasma) PersistAll() {
 	retry:
 		pg := s.ReadPage(pid).(*page)
 		bs := pg.Marshal(buf)
-		offset, wbuf, res := s.lss.ReserveSpace(len(bs))
+		offset, wbuf, res := s.lss.ReserveSpace(lssBlockTypeSize + len(bs))
 		offsetPtr := pg.addFlushDelta()
 		if !s.UpdateMapping(pid, pg) {
 			// discard lss space
+			binary.BigEndian.PutUint16(wbuf[:lssBlockTypeSize], uint16(lssDiscard))
 			s.lss.FinalizeWrite(res)
 			goto retry
 		}
 
-		copy(wbuf, bs)
+		copy(wbuf[lssBlockTypeSize:], bs)
 		s.lss.FinalizeWrite(res)
 		*offsetPtr = offset
 
