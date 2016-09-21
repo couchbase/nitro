9c03206f9bfb8091c98a0b8e78a1848d8ffdfa3a
Sarath Lakshman <sarathlakshman@slynux.com>
2016-09-21 19:37:02 +0530

    plasma/lss: Add test for lss superblock
    Change-Id: I9fce7aa70d60520a7ae1e88b30874ab899905a8d

--- plasma/lss_test.go
+++ plasma/lss_test.go
@@ -104,3 +104,50 @@ func TestLSSCleaner(t *testing.T) {
 		copy(bufread[:8], empty)
 	}
 }
+
+func TestLSSSuperBlock(t *testing.T) {
+	maxSize := int64(1024 * 1024 * 20)
+	BufSize := 1024 * 1024
+	nbuffers := 2
+	freeSpace := int64(5 * 1024 * 1024)
+
+	os.Remove("test.data")
+	lss, _ := newLSStore("test.data", maxSize, BufSize, nbuffers)
+	n := 100000
+
+	go func() {
+		buf := make([]byte, 1024*1024)
+		for {
+			if lss.AvailableSpace() < freeSpace {
+				lss.RunCleaner(func(off lssOffset, bs []byte) bool {
+					if lss.AvailableSpace() >= freeSpace {
+						return false
+					}
+
+					return true
+				}, buf)
+			} else {
+				time.Sleep(time.Second)
+			}
+		}
+	}()
+
+	for i := 0; i < n; i++ {
+		_, buf, res := lss.ReserveSpace(1024)
+		binary.BigEndian.PutUint64(buf[:8], uint64(i))
+		lss.FinalizeWrite(res)
+	}
+
+	tail := lss.tailOffset
+	head := lss.headOffset
+	lss.Close()
+
+	lss, _ = newLSStore("test.data", maxSize, BufSize, nbuffers)
+	if tail != lss.tailOffset {
+		t.Errorf("tail: expected %d, got %d", tail, lss.tailOffset)
+	}
+
+	if head != lss.headOffset {
+		t.Errorf("head: expected %d, got %d", tail, lss.tailOffset)
+	}
+}
