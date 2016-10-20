463811e4f8acb0cbd0514dc73fdd65ee3ca7fcea
Sarath Lakshman <sarathlakshman@slynux.com>
2016-10-20 06:56:20 +0530

    plasma: Implement concurrent plasma page visitor
    Change-Id: Ibe562588007e1898e5b230eaabfb4a81bf7eeb0e

---
+++ plasma/page_visitor.go
@@ -0,0 +1,82 @@
+package plasma
+
+import (
+	"github.com/t3rm1n4l/nitro/skiplist"
+	"sync"
+	"unsafe"
+)
+
+type PageVisitorCallback func(pid PageId, partn RangePartition) error
+
+type RangePartition struct {
+	Shard  int
+	MinKey unsafe.Pointer
+	MaxKey unsafe.Pointer
+}
+
+func (s *Plasma) PageVisitor(callb PageVisitorCallback, concurr int) error {
+	var wg sync.WaitGroup
+	partitions := s.GetRangePartitions(concurr)
+	errors := make([]error, len(partitions))
+
+	for _, partn := range partitions {
+		wg.Add(1)
+		go func(p RangePartition) {
+			defer wg.Done()
+			errors[p.Shard] = s.VisitPartition(p, callb)
+		}(partn)
+	}
+
+	wg.Wait()
+
+	for _, err := range errors {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Plasma) VisitPartition(partn RangePartition, callb PageVisitorCallback) error {
+	buf := s.Skiplist.MakeBuf()
+	itr := s.Skiplist.NewIterator(s.cmp, buf)
+
+	if partn.MinKey == skiplist.MinItem {
+		pid := s.StartPageId()
+		if err := callb(pid, partn); err != nil {
+			return err
+		}
+	}
+
+	for itr.Seek(partn.MinKey); itr.Valid() && s.cmp(itr.Get(), partn.MaxKey) < 0; itr.Next() {
+		pid := PageId(itr.GetNode())
+		if err := callb(pid, partn); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Plasma) GetRangePartitions(n int) []RangePartition {
+	var partns []RangePartition
+	var shard int
+
+	barrier := s.Skiplist.GetAccesBarrier()
+	token := barrier.Acquire()
+	defer barrier.Release(token)
+
+	partns = append(partns, RangePartition{MinKey: skiplist.MinItem})
+	for _, key := range s.Skiplist.GetRangeSplitItems(n) {
+		if s.cmp(key, partns[shard].MinKey) > 0 {
+			key = s.dup(key)
+			partns[shard].MaxKey = key
+			partns = append(partns, RangePartition{MinKey: key, Shard: shard})
+			shard++
+		}
+	}
+
+	partns[shard].MaxKey = skiplist.MaxItem
+	return partns
+}
