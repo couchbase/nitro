05ca5b58242af77197f28a8b6e407796dcb7ca4d
Sarath Lakshman <sarathlakshman@slynux.com>
2017-02-28 00:15:34 +0530

    plasma: Add a test for larger-than-memory recovery
    Change-Id: I31f91b756db050650ef4754b6d20367111532133

--- plasma/gm_test.go
+++ plasma/gm_test.go
@@ -29,7 +29,6 @@ func TestPlasmaGreaterThanMemoryPerf(t *testing.T) {
 	iterations := 5
 	nPerThr := n / numThreads
 	cfg := testSnCfg
-	cfg.TriggerSwapper = QuotaSwapper
 	cfg.UseMemoryMgmt = true
 	cfg.AutoSwapper = true
 	s := newTestIntPlasmaStore(cfg)
@@ -115,4 +114,56 @@ func TestPlasmaGreaterThanMemoryPerf(t *testing.T) {
 
 	close(stopch)
 
+}
+
+func TestGMRecovery(t *testing.T) {
+	defer SetMemoryQuota(maxMemoryQuota)
+
+	SetMemoryQuota(5 * 1024 * 1024)
+	os.RemoveAll("teststore.data")
+	cfg := testSnCfg
+	cfg.UseMemoryMgmt = true
+	cfg.AutoSwapper = true
+	s := newTestIntPlasmaStore(cfg)
+	w := newTestWriter(s.NewWriter())
+
+	n := 1000000
+	for i := 0; i < n; i++ {
+		w.InsertKV([]byte(fmt.Sprintf("key-%10d", i)), []byte(fmt.Sprintf("val-%10d", i)))
+		w.numOps++
+		if i%1000 == 0 {
+			snap := s.NewSnapshot()
+			snap.Close()
+		}
+	}
+
+	snap := s.NewSnapshot()
+	snap.Open()
+	s.CreateRecoveryPoint(snap, []byte("rp"))
+
+	for _, rpt := range s.GetRecoveryPoints() {
+		fmt.Printf("recovery_point sn:%d meta:%s\n", rpt.sn, string(rpt.meta))
+	}
+
+	s.Close()
+
+	fmt.Println("Reopening database...")
+	s = newTestIntPlasmaStore(cfg)
+
+	rpts := s.GetRecoveryPoints()
+	rb := rpts[0]
+	snap, _ = s.Rollback(rb)
+	fmt.Println("Rollbacked to", string(rb.meta))
+
+	fmt.Println(s.GetStats())
+	itr := snap.NewIterator()
+	count := 0
+	for itr.SeekFirst(); itr.Valid(); itr.Next() {
+		count++
+	}
+
+	if count != n {
+		t.Errorf("Expected %d, got %d", n, count)
+	}
+	s.Close()
 }
